47eb0133915af80919e84eacf838521400bf6052
Sudarshan Sujay" Yadala <sujayyadalam@royal-03.cs.wisc.edu>
2019-11-21 12:27:21 -0600

    Need to free ptr[p].

--- mapreduce.c
+++ mapreduce.c
@@ -1,3 +1,7 @@
+/*
+ *  Copyright 2019 SujayYadalam BobbiYogatama
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -6,18 +10,16 @@
 #include <semaphore.h>
 #include "mapreduce.h"
 
-struct vnode
-{
-	char *value;
-	struct vnode *next;
+struct vnode {
+    char *value;
+    struct vnode *next;
 };
 
-struct kvpair
-{
-	char *key;
-	struct vnode *vn;
-	struct vnode *current;
-	struct kvpair *next;
+struct kvpair {
+    char *key;
+    struct vnode *vn;
+    struct vnode *current;
+    struct kvpair *next;
 };
 
 Mapper fp_map;
@@ -27,19 +29,19 @@ Partitioner fp_part;
 struct kvpair **part;
 char **files;
 int n_partitions;
-int next_file = 1; // Files start from argv[1]
+int next_file = 1;  // Files start from argv[1]
 int next_partition = 0;
 
 pthread_mutex_t file_lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t part_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t *partition_lock;
 
-void map_pool(void* args)
-{
+void map_pool(void* args) {
     char *file;
 
-    while(files[next_file] != NULL && strlen(files[next_file]) > 0) {
+    while (files[next_file] != NULL && strlen(files[next_file]) > 0) {
         pthread_mutex_lock(&file_lock);
-        if(files[next_file]) {
+        if (files[next_file]) {
             file = files[next_file++];
             pthread_mutex_unlock(&file_lock);
             fp_map(file);
@@ -49,130 +51,126 @@ void map_pool(void* args)
     }
 }
 
-void reduce_pool(void* args)
-{
-	struct kvpair *ptr, *iter;
+void reduce_pool(void* args) {
+    struct kvpair *ptr, *temp;
     int p;
-	
-    while(next_partition < n_partitions) {
+
+    while (next_partition < n_partitions) {
         pthread_mutex_lock(&part_lock);
-        if(next_partition < n_partitions) {    	
+        if (next_partition < n_partitions) {
             p = next_partition++;
             pthread_mutex_unlock(&part_lock);
             ptr = part[p];
-		    while(ptr != NULL) {
-			    fp_reduce(ptr->key, get_next, p);
-		    	ptr = ptr->next;
-			    } 	
-	    } else {
+            while (ptr != NULL) {
+                fp_reduce(ptr->key, get_next, p);
+                ptr = ptr->next;
+                // TODO(sujay): need to free part[p]
+                while(part[p]->vn) {
+                    temp = part[p]->vn;
+                    part[p]->vn = part[p]->vn->next;
+                    free(temp->value);
+                    free(temp);
+                }
+                free(part[p]->key);
+                free(part[p]);
+                part[p] = ptr;
+            }
+        } else {
             pthread_mutex_unlock(&part_lock);
         }
     }
 }
 
-void insert_value(struct kvpair *key_ptr, char *value)
-{
-	struct vnode *ptr, *prev = NULL, *new;
-	new = malloc(sizeof(struct vnode));
-	new->value = strdup(value);
-	new->next = NULL;
+void MR_Emit(char *key, char *value) {
+    struct kvpair *ptr, *prev = NULL, *new;
+    int p;
 
-	ptr = key_ptr->vn;
+    p = fp_part(key, n_partitions);
 
-	if(ptr == NULL) {
-		key_ptr->vn = new;
-		key_ptr->current = new;
-		return;
-	}
+    pthread_mutex_lock(&partition_lock[p]);
+    ptr = part[p];
+    if (ptr == NULL) {
+        new = malloc(sizeof(struct kvpair));
+        new->key = strdup(key);
+        new->next = NULL;
 
-	while(ptr != NULL && (strcmp(ptr->value, value) < 0)) {
-		prev = ptr;
-		ptr = ptr->next;
-	}
-	new->next = ptr;
-	if(prev != NULL)
-		prev->next = new;
-	else {
-		key_ptr->vn = new;
-		key_ptr->current = new;
-	}
-}
+        struct vnode *newv = malloc(sizeof(struct vnode));
+        newv->value = strdup(value);
+        newv->next = new->vn;
+        new->vn = newv;
+        new->current = newv;
 
-void MR_Emit(char *key, char *value)
-{
-	struct kvpair *ptr, *prev=NULL, *new;
-	int p;
+        part[p] = new;
+        goto end;
+    }
 
-	p = fp_part(key, n_partitions);
+    while (ptr != NULL) {
+        int cmp = strcmp(ptr->key, key);
+        if (cmp == 0) {
+            struct vnode *newv = malloc(sizeof(struct vnode));
+            newv->value = strdup(value);
+            newv->next = ptr->vn;
+            ptr->vn = newv;
+            ptr->current = newv;
+            goto end;
+        } else if (cmp > 0) {
+            break;
+        }
+        prev = ptr;
+        ptr = ptr->next;
+    }
 
-	pthread_mutex_lock(&part_lock);
-	ptr = part[p];
-	if(ptr == NULL) {
-		new = malloc(sizeof(struct kvpair));
-		new->key = strdup(key);
-		new->next = NULL;
-		insert_value(new, value);
+    new = malloc(sizeof(struct kvpair));
+    new->key = strdup(key);
+    new->next = ptr;
+    if (prev != NULL)
+        prev->next = new;
+    else
+        part[p] = new;
 
-		part[p] = new;
-		goto end;
-	}
-
-	while(ptr != NULL) {
-		int cmp = strcmp(ptr->key, key);
-		if(cmp == 0) {
-			insert_value(ptr, value);
-			goto end;
-		} else if(cmp > 0) {
-			break;
-		}
-		prev = ptr;
-		ptr = ptr->next;
-	}
-
-	new = malloc(sizeof(struct kvpair));
-	new->key = strdup(key);
-	new->next = ptr;
-	if(prev != NULL)
-		prev->next = new;
-	else
-		part[p] = new;
-	insert_value(new, value);
+    struct vnode *newv = malloc(sizeof(struct vnode));
+    newv->value = strdup(value);
+    newv->next = new->vn;
+    new->vn = newv;
+    new->current = newv;
 
 end:
-	pthread_mutex_unlock(&part_lock);
+    pthread_mutex_unlock(&partition_lock[p]);
 }
 
-char* get_next(char *key, int partition_number)
-{
-	struct kvpair *ptr;
-	char *temp;
+char* get_next(char *key, int partition_number) {
+    struct kvpair *ptr;
+    char *temp;
 
-	ptr = part[partition_number];
-	if (ptr == NULL)
-		return NULL;
-	
-	while(ptr != NULL) {
-		if(strcmp(ptr->key, key) == 0) {
-			
-			if(ptr->current == NULL)
-				goto end;
-			temp = ptr->current->value;
-			ptr->current = ptr->current->next;
-			return temp;
-		}
-		ptr = ptr->next;
-	}
+    ptr = part[partition_number];
+    if (ptr == NULL)
+        return NULL;
+
+    while (ptr != NULL) {
+        // if(strcmp(ptr->key, key) == 0) {
+        if (ptr->key == key) {
+            if (ptr->current == NULL)
+                return NULL;
+            temp = ptr->current->value;
+            ptr->current = ptr->current->next;
+            return temp;
+        }
+        ptr = ptr->next;
+    }
 
 end:
-	return NULL;
+    return NULL;
 }
 
-unsigned long Log2n(unsigned long n){
-    return (n > 1) ? 1 + Log2n(n/2) : 0;
+unsigned int Log2n(unsigned int input) {
+    unsigned int result = 0;
+    while (input >>= 1) {
+        result++;
+    }
+    return result;
 }
 
-unsigned long MR_DefaultHashPartition(char *key, int num_partitions)
-{
+unsigned long MR_DefaultHashPartition(char *key, int num_partitions) {
     unsigned long hash = 5381;
     int c;
     while ((c = *key++) != '\0')
@@ -180,62 +178,49 @@ unsigned long MR_DefaultHashPartition(char *key, int num_partitions)
     return hash % num_partitions;
 }
 
-unsigned long MR_SortedPartition(char *key, int num_partitions)
-{
-	unsigned long temp = atoi(key);
-	temp = temp & 0x00000000ffffffff;
-	temp = temp >> (32 - Log2n(num_partitions));
-	return temp;
+unsigned long MR_SortedPartition(char *key, int num_partitions) {
+    unsigned long temp = atoi(key);
+    temp = temp & 0x00000000ffffffff;
+    temp = temp >> (32 - Log2n(num_partitions));
+    return temp;
 }
 
-
-void MR_Run(int argc, char *argv[], 
-	    Mapper map, int num_mappers, 
-	    Reducer reduce, int num_reducers, 
-	    Partitioner partition, int num_partitions)
-{
+void MR_Run(int argc, char *argv[],
+        Mapper map, int num_mappers,
+        Reducer reduce, int num_reducers,
+        Partitioner partition, int num_partitions) {
     int i;
 
-	pthread_t mthread[num_mappers], rthread[num_reducers];
+    pthread_t mthread[num_mappers], rthread[num_reducers];
 
-	n_partitions = num_partitions;
+    n_partitions = num_partitions;
     files = argv;
 
     fp_map = map;
-	fp_reduce = reduce;
-	fp_part = partition;
+    fp_reduce = reduce;
+    fp_part = partition;
 
     part = (struct kvpair **)malloc(n_partitions * sizeof(struct kvpair *));
+    partition_lock = (pthread_mutex_t*)
+                     malloc(n_partitions * sizeof(pthread_mutex_t));
 
-    for(i=0; i<num_mappers; i++) {
-    	pthread_create(&mthread[i], NULL, (void *)map_pool, NULL);
+    for (i=0; i < num_partitions; i++)
+        pthread_mutex_init(&partition_lock[i], NULL);
+
+    for (i=0; i < num_mappers; i++) {
+        pthread_create(&mthread[i], NULL, (void *)map_pool, NULL);
     }
-    for(i=0; i<num_mappers; i++) {
+    for (i=0; i < num_mappers; i++) {
         pthread_join(mthread[i], NULL);
     }
 
-    for(i=0; i<num_reducers; i++) {
-    	pthread_create(&rthread[i], NULL, (void *)reduce_pool, NULL);
+    for (i=0; i < num_reducers; i++) {
+        pthread_create(&rthread[i], NULL, (void *)reduce_pool, NULL);
     }
-    for(i=0; i<num_reducers; i++) {
+    for (i=0; i < num_reducers; i++) {
         pthread_join(rthread[i], NULL);
     }
 
-    for(i=0; i<n_partitions; i++) {
-        struct kvpair *ptr = part[i];
-        struct kvpair *iter;
-        
-        while(ptr != NULL) {
-            struct vnode *temp;
-            while(ptr->vn) {
-            	temp = ptr->vn;
-            	ptr->vn = ptr->vn->next;
-            	free(temp->value);
-            	free(temp);
-            }
-            iter = ptr;
-            ptr = ptr->next;
-            free(iter);
-        }
-    }
+    free(part);
+    free(partition_lock);
 }
